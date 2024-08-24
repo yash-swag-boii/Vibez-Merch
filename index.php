afa024c2b6af4af9b95b62e18527979cdc90377f
prabodchamika <157189856+prabodchamika@users.noreply.github.com>
2024-08-24 21:20:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Vibez-Mearch</title>
+</head>
+
+<body>
+    <header id="header">
+        <div class="header-top-bar">
+            <span>Welcome to Vibez-Mearch</span>
+        </div>
+
+        <nav class="navigation">
+            <a href="#" class="logo">
+                <img src="" alt="logo" />
+            </a>
+
+            <ul class="menu">
+                <li><a href="" class="active">Home</a></li>
+                <li><a href="">Home</a></li>
+                <li><a href="">Home</a></li>
+                <li><a href="">Home</a></li>
+                <li><a href="">Home</a></li>
+            </ul>
+
+        </nav>
+
+    </header>
+
+
+
+
+    <nav class="navigation">
+
+
+
+
+        </header>
+
+</body>
+
+</html>
